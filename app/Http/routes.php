63ef2626074e2af82a17262ec22035d77a305222
Haoquan Li <haoqli.cs@gmail.com>
2016-04-12 19:12:57 -0400

    add profile page

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,8 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::auth();
+Route::group(['middleware' => ['web']], function () {
+    Route::auth();
+});
 
-Route::get('/home', 'HomeController@index');
+Route::group(['middleware' => ['web', 'auth']], function () {
+    Route::get('/', 'TimelineController@index');
 
-Route::get('{name}', array('as' => 'profile', 'uses' => 'UserController@showProfile'));
+    Route::get('/posts', 'PostController@index');
+    Route::post('/posts', 'PostController@create');
+
+    Route::get('/users/{user}', 'UserController@index')->name('user.index');
+    Route::get('/users/{user}/follow', 'UserController@follow')->name('user.follow');
+    Route::get('/users/{user}/unfollow', 'UserController@unfollow')->name('user.unfollow');
+
+});
